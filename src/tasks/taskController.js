04051141ce0bdced43bb4e076f57095a6a84546e
harrisch <charlie.harris@wwt.com>
2017-07-21 23:19:00 -0500

    Discovered what mongodb population looks like

--- src/tasks/taskController.js
+++ src/tasks/taskController.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 
 const co = require('co')
 const mongoose = require('mongoose')
@@ -6,36 +6,35 @@ const _ = require('lodash')
 const Boom = require('boom')
 
 module.exports = function (Task, log) {
-
   return {
-		list: co.wrap(listTasks),
+    list: co.wrap(listTasks),
     patch: co.wrap(patchTask),
     deactivateTask: co.wrap(deactivateTask),
     deleteTask: co.wrap(deleteTask),
     create: co.wrap(createTask),
-    getTask: co.wrap(getTask),
+    getTask: co.wrap(getTask)
   }
 
-  function* getTask(request, reply) {
+  function * getTask (request, reply) {
     const task = yield Task.findById(request.params.taskId)
       .populate('children')
 
     reply(task)
   }
 
-  function* listTasks(request, reply) {
+  function * listTasks (request, reply) {
     const result = yield Task.find({
-      isActive: true, 
+      isActive: true,
       owner: new mongoose.Types.ObjectId(request.auth.credentials.id)
     })
 
     reply(result)
-	}
+  }
 
-	function* createTask(request, reply) {
+  function * createTask (request, reply) {
     const task = new Task(request.payload)
 
-    let result 
+    let result
     try {
       result = yield task.save()
     } catch (e) {
@@ -43,24 +42,24 @@ module.exports = function (Task, log) {
     }
 
     reply(result)
-	}
+  }
 
-  function* patchTask(request, reply) {
+  function * patchTask (request, reply) {
     const task = yield Task.findById(request.params.taskId)
-    _.extend(task, request.payload)    
+    _.extend(task, request.payload)
 
     const result = yield task.save()
 
     reply(result)
   }
 
-  function* deactivateTask(request, reply) {
+  function * deactivateTask (request, reply) {
     const task = yield Task.findByIdAndUpdate(request.params.taskId, { isActive: false }, { new: true })
 
     reply(task)
   }
 
-  function* deleteTask(request, reply) {
+  function * deleteTask (request, reply) {
     // TODO: deprecated. Make Service method only
     yield Task.findByIdAndRemove(request.params.taskId)
 
--- src/users/userController.js
+++ src/users/userController.js
@@ -1,49 +1,48 @@
-"use strict";
+'use strict'
 
 const co = require('co')
 const _ = require('lodash')
 
 module.exports = function (User, log) {
-
   return {
-		list: co.wrap(list),
+    list: co.wrap(list),
     update: co.wrap(update),
     deleteUser: co.wrap(deleteUser),
     create: co.wrap(create),
-    getOneByAuthToken: co.wrap(getOneByAuthToken),
+    getOneByAuthToken: co.wrap(getOneByAuthToken)
   }
 
-  function* list(request, reply) {
+  function * list (request, reply) {
     const result = yield User.find()
 
     reply(result)
-	}
+  }
 
-	function* create(request, reply) {
+  function * create (request, reply) {
     const user = new User({
       firstName: request.payload.firstName,
       lastName: request.payload.lastName,
-      email: request.payload.email,
+      email: request.payload.email
     })
 
     const result = user.save()
 
     reply(result)
-	}
+  }
 
-  function* update(request, reply) {
+  function * update (request, reply) {
     const user = yield User.findByIdAndUpdate(request.params.userId, request.payload, { new: true })
 
     reply(user)
   }
 
-  function* deleteUser(request, reply) {
+  function * deleteUser (request, reply) {
     reply(`application "${request.params.userId}" deleted!`)
   }
 
-  function* getOneByAuthToken(request, reply) {
+  function * getOneByAuthToken (request, reply) {
     const user = yield User.findById(request.auth.credentials.id)
-      .populate({ path: 'lifeTask', populate: { path: 'children' }})
+      .populate('lifeTask')
 
     reply(user)
   }
